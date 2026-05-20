d2d3572fc23decc769f8d27c6f391d7a31ff21b1
Eva <eva@100yen.org>
2026-05-20 17:58:29 +0700

    feat: connect workbench auth broker

--- apps/eva-desktop-mac/Sources/EvaDesktopCore/Models/DesktopSession.swift
+++ apps/eva-desktop-mac/Sources/EvaDesktopCore/Models/DesktopSession.swift
@@ -17,13 +17,58 @@ public struct DesktopSession: Codable, Equatable, Sendable {
     }
 }
 
+public enum EvaDesktopISO8601 {
+    public static func parse(_ value: String) -> Date? {
+        let fractional = ISO8601DateFormatter()
+        fractional.formatOptions = [.withInternetDateTime, .withFractionalSeconds]
+        if let date = fractional.date(from: value) {
+            return date
+        }
+
+        let standard = ISO8601DateFormatter()
+        return standard.date(from: value)
+    }
+
+    public static func decoder() -> JSONDecoder {
+        let decoder = JSONDecoder()
+        decoder.dateDecodingStrategy = .custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let value = try container.decode(String.self)
+            if let date = parse(value) {
+                return date
+            }
+            throw DecodingError.dataCorruptedError(
+                in: container,
+                debugDescription: "Expected ISO-8601 timestamp"
+            )
+        }
+        return decoder
+    }
+}
+
 public struct RuntimeLaunchRequest: Codable, Equatable, Sendable {
+    public let action: String
     public let customerId: String
     public let runtime: RuntimeKey
 
     public init(customerId: String, runtime: RuntimeKey) {
+        self.action = "runtime_launch"
         self.customerId = customerId
         self.runtime = runtime
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case action
+        case customerId = "customer_id"
+        case runtime
+    }
+}
+
+public struct DesktopSessionRevokeRequest: Codable, Equatable, Sendable {
+    public let action: String
+
+    public init() {
+        self.action = "revoke_desktop_session"
     }
 }
 
