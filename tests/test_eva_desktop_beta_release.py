1ab45ba2108ecdc76c357d9ed129947fa503f7c1
Eva <admin@100yen.org>
2026-05-30 07:38:52 +0700

    Cut Workbench 0.6.8 release (#170)
    * Cut Workbench 0.6.8 release
    
    * Update release smoke for 0.6.8
    
    ---------
    
    Co-authored-by: Eva <eva@100yen.org>

--- tests/test_eva_desktop_beta_release.py
+++ tests/test_eva_desktop_beta_release.py
@@ -12,10 +12,10 @@ def test_beta_packaging_uses_no_developer_id_path() -> None:
     app_brand = (APP_ROOT / "Sources" / "EvaDesktopCore" / "Models" / "AppBrand.swift").read_text(encoding="utf-8")
 
     assert "--package-beta" in script
-    assert 'VERSION="0.6.7"' in script
-    assert 'BUILD_NUMBER="47"' in script
-    assert 'version = "0.6.7"' in app_brand
-    assert 'buildNumber = "47"' in app_brand
+    assert 'VERSION="0.6.8"' in script
+    assert 'BUILD_NUMBER="48"' in script
+    assert 'version = "0.6.8"' in app_brand
+    assert 'buildNumber = "48"' in app_brand
     assert 'REQUIRED_PEEKABOO_VERSION="${EVAOS_REQUIRED_PEEKABOO_VERSION:-3.2.2 or newer}"' in script
     assert 'STRICT_PEEKABOO_CHECK="${EVAOS_STRICT_PEEKABOO_CHECK:-1}"' in script
     assert 'STRICT_PEEKABOO_CHECK="${EVAOS_STRICT_PEEKABOO_CHECK:-0}"' in script
